d2f6d2f9b944c2930feccd21cf5b2a9977595773
Somaia Khalil <sumaya_khair@live.com>
2020-01-28 02:58:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
-class Dog 
-  def name= (dog_name)
+class Dog
+  def name=(dog_name)
     this_dogs_name = dog_name
-  end 
-  def name 
+  end
+ 
+  def name
     this_dogs_name
-  end 
-end 
+  end
+end
 
 lassie = Dog.new 
 lassie.name = "lassie"
